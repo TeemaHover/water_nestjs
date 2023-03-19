d734145f6c27159b719d9afbba2a641f340b5775
dorjoo <dorjoohover@gmail.com>
2023-03-20 01:46:51 +0800

    user lawyer

--- src/resourse/order/order.module.ts
+++ src/resourse/order/order.module.ts
@@ -1,13 +1,13 @@
 import { Module } from "@nestjs/common";
 
 import { MongooseModule } from "@nestjs/mongoose";
-import { Order, OrderSchema } from "src/schema";
+import { Order, OrderSchema, User, UserSchema } from "src/schema";
 import { OrderController } from "./order.controller";
 import { OrderService } from "./order.service";
 
 
 @Module({
-    imports: [ MongooseModule.forFeature([{name: Order.name, schema: OrderSchema}])],
+    imports: [ MongooseModule.forFeature([{name: Order.name, schema: OrderSchema}, {name: User.name, schema: UserSchema}])],
     controllers: [OrderController],
     providers: [OrderService],
     exports: [OrderService]
--- src/resourse/order/order.service.ts
+++ src/resourse/order/order.service.ts
@@ -1,17 +1,17 @@
 import { HttpException, Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
-import { Order, OrderDocument } from "src/schema";
+import { Order, OrderDocument, User, UserDocument } from "src/schema";
 import { ServiceStatus } from "src/utils/enum";
 
 @Injectable()
 export class OrderService {
-  constructor(@InjectModel(Order.name) private model: Model<OrderDocument>){}
+  constructor(@InjectModel(Order.name) private model: Model<OrderDocument>, @InjectModel(User.name) private userModel: Model<UserDocument>){}
 
 
   async getUserOrders(id: string) {
     try {
-      let orders = await this.model.find({$or: [{lawyerId: id}, {clientId: id}]})
+      let orders = await this.model.find({$or: [{lawyerId: id}, {clientId: id}]}).populate('clientId', 'firstname lastname phone ', this.userModel  ).populate('lawyerId', 'firstname lastname phone profileImg', this.userModel  )
       return orders
     } catch (error) {
       throw new HttpException(error, 500)
