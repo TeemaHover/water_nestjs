e7ca3ccd7eaa915bd522db7fa7c412b91e4d9ca9
dorjoo <dorjoohover@gmail.com>
2023-05-06 04:26:55 +0800

    'asdf'

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -2,8 +2,4 @@ export default () => ({
   appSecret: process.env.APP_SECRET,
   dbUrl: process.env.DB_URL,
   dbName: process.env.DB_NAME,
-  s3accessKeyId: process.env.S3ACCESSKEYID,
-  s3secretAccessKey: process.env.S3SECRETACCESSKEY,
-  s3bucket: process.env.S3BUCKET,
-  s3region: process.env.S3REGION,
 })
