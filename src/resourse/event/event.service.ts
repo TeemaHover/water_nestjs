5f7b6bffb768c22912f973f9775ac9ec001da90e
dorjoo <dorjoohover@gmail.com>
2023-05-11 03:05:56 +0800

    'asdf'

--- src/resourse/event/event.service.ts
+++ src/resourse/event/event.service.ts
@@ -1,6 +1,6 @@
 import { HttpException, Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
-import { Model } from 'mongoose';
+import mongoose, { Model } from 'mongoose';
 import { Event, EventDocument } from "src/schema";
 import { UserType } from "src/utils/enum";
 import { EventDto } from "./event.dto";
@@ -11,7 +11,8 @@ export class EventService {
     try {
       return await this.model.create({
         ...dto,
-        business: user
+        business: user,
+        voluntary: mongoose.Types.ObjectId(dto.voluntary),
 
       })
     } catch (error) {
