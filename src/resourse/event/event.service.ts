2bf62d929b16821c5cc6799a596fd4e1a6218b3a
dorjoo <dorjoohover@gmail.com>
2023-05-11 00:15:05 +0800

    'asdf'

--- src/resourse/event/event.service.ts
+++ src/resourse/event/event.service.ts
@@ -30,8 +30,8 @@ export class EventService {
     try {
       let event = await this.model.findById(id)
       if(!event) throw new HttpException("Event not found", 404)
-      console.log(event.registerMembers+2)
-      if(event.members >= event.registerMembers+2) throw new HttpException("Event is full", 400)
+      
+      if(event.members >= event.registerMembers+1) throw new HttpException("Event is full", 400)
       event =  await this.model.findByIdAndUpdate(id, { $inc: { registerMembers: 1 }, $push : {users: user} })
       if(event) return true
       else return false
